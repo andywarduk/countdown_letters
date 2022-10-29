2fe10d6266bca43f56aded620a68e21ab16e25e3
Andy Ward.uk <andy.ward.uk@gmail.com>
2022-10-29 11:46:29 +0100

    optimise #3

--- src/solver.rs
+++ src/solver.rs
@@ -5,52 +5,63 @@ use crate::dictionary::{LetterNext, LetterVec};
 pub fn find_words(letters: &str, dictionary: &Vec<LetterVec>, min_len: u8) -> Vec<String> {
     let mut result = HashSet::new();
 
-    let letter_chars = letters.as_bytes();
+    // Dictionary entry element numbers for each letter
+    let letter_elems = letters.chars().map(|c| c as u8 - b'A').collect::<Vec<u8>>();
+
+    // Vector of chosen letter elements
+    let mut chosen = Vec::with_capacity(letters.len());
+
+    // Been chosen indicators
     let mut is_chosen = vec![false; letters.len()];
 
+    // Start search recursion
     find_words_rec(
-        &mut Vec::with_capacity(letters.len()),
-        0,
-        letter_chars,
+        &mut chosen,
         &mut is_chosen,
+        &letter_elems,
         dictionary,
+        0,
         min_len,
         &mut result,
     );
 
+    // Convert hash set to vector
     result.into_iter().collect()
 }
 
 fn find_words_rec(
     chosen: &mut Vec<u8>,
-    elem: usize,
-    letters: &[u8],
     is_chosen: &mut Vec<bool>,
+    letter_elems: &[u8],
     dictionary: &Vec<LetterVec>,
+    dict_elem: usize,
     min_len: u8,
     result: &mut HashSet<String>,
 ) {
-    for i in 0..letters.len() {
+    for i in 0..letter_elems.len() {
         if is_chosen[i] {
             // This letter has already been chosen
             continue;
         }
 
-        // Get chosen character
-        let chosen_char = letters[i];
+        // Get chosen letter element
+        let chosen_letter = letter_elems[i];
 
         // Add it to the chosen list
-        chosen.push(chosen_char);
+        chosen.push(chosen_letter);
 
         // Walk the dictionary
-        let dict_elem = dictionary[elem][uchar_to_elem(chosen_char)];
+        let dict_elem = dictionary[dict_elem][chosen_letter as usize];
 
         // End of a word?
         match dict_elem {
             LetterNext::End | LetterNext::EndNext(_) => {
                 if chosen.len() >= min_len as usize {
                     // SAFETY: Guaranteed to be upper case ASCII characters only
-                    let string = unsafe { String::from_utf8_unchecked(chosen.to_vec()) };
+                    let string = chosen
+                        .iter()
+                        .map(|e| (*e + b'A') as char)
+                        .collect::<String>();
                     result.insert(string);
                 }
             }
@@ -61,21 +72,25 @@ fn find_words_rec(
         match dict_elem {
             LetterNext::Next(e) | LetterNext::EndNext(e) => {
                 is_chosen[i] = true;
+
                 find_words_rec(
-                    chosen, e as usize, letters, is_chosen, dictionary, min_len, result,
+                    chosen,
+                    is_chosen,
+                    letter_elems,
+                    dictionary,
+                    e as usize,
+                    min_len,
+                    result,
                 );
+                
                 is_chosen[i] = false;
             }
             _ => (),
         }
 
+        // SAFETY: length always decreasing and always removing the pushed entry above
         unsafe {
             chosen.set_len(chosen.len() - 1);
         }
     }
-}
-
-#[inline]
-fn uchar_to_elem(c: u8) -> usize {
-    (c as u8 - b'A') as usize
 }
